e2c01ed0d74a54384604244adc697b326d509b3c
HungTrinhJH <hung.trinh@journeyh.io>
2021-11-02 11:34:45 +0700

    Update Activity Feed

--- src/components/ActivityFeed/ActivityFeed.js
+++ src/components/ActivityFeed/ActivityFeed.js
@@ -137,6 +137,22 @@ const resolveTransitionMessage = (
       ) : (
         <FormattedMessage id="ActivityFeed.transitionAccept" values={{ displayName }} />
       );
+    case TRANSITION_CANCEL_BOOKING_BEFORE_ACCEPTED ||
+      TRANSITION_CANCEL_BOOKING_AFTER_ACCEPTED ||
+      TRANSITION_CANCEL_WITH_FULL_REFUND ||
+      TRANSITION_CANCEL_WITH_NO_REFUND:
+      return isOwnTransition ? (
+        <FormattedMessage id="ActivityFeed.OwnTransitionCancelByCustomer" />
+      ) : (
+        <FormattedMessage id="ActivityFeed.transitionCancelByCustomer" values={{ displayName }} />
+      );
+
+    case TRANSITION_CANCEL_BY_PROVIDER:
+      return isOwnTransition ? (
+        <FormattedMessage id="ActivityFeed.ownTransitionCancelByProvider" />
+      ) : (
+        <FormattedMessage id="ActivityFeed.transitionCancelByProvider" values={{ displayName }} />
+      );
     case TRANSITION_DECLINE:
       return isOwnTransition ? (
         <FormattedMessage id="ActivityFeed.ownTransitionDecline" />
